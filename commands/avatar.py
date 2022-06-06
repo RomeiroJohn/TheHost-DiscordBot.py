835d1a5a8de2d1855017697e39538052105c46b0
RomeiroJohn <romeiro.john@gmail.com>
2022-06-06 10:43:18 -0300

    The host

---
+++ commands/avatar.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+import discord
+
+class avatar(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(name="avatar")
+    async def showavatar(self,msg):
+        embed = discord.Embed(title="This is your avatar")
+        embed.set_author(name=msg.author.name)
+        embed.set_image(url=msg.author.avatar_url)
+        await msg.send(embed=embed)
+        
+
+
+
+
+def setup(bot):
+    bot.add_cog(avatar(bot))
--- commands/embeds.py
+++ commands/embeds.py
@@ -16,8 +16,13 @@ class embeds(commands.Cog):
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/702019138033549312/980990345317855262/WhatsApp_Image_2022-05-30_at_21.24.16.jpeg")
         embed.set_author(name="The Host")
         embed.set_footer(text="J.#5211", icon_url="https://cdn.discordapp.com/avatars/208652904909766658/3a55bc4af8ce9eee26b984e89e986e2c.webp?size=32")
-        embed.add_field(name="$info",value="send this list to the user",inline=False)
-        embed.add_field(name="$limpar 'Ammount'", value="This command can clear the chat by passing a quantity", inline=False)
+        embed.add_field(name="All The Host commands",value="""
+        *$info = send this message to the user
+        $limpar **(amount)** = self clean the chat by passing a value
+        $createtextchannel **(channelname)** = create a basic text channel
+        $createvoicechannel **(channelname)** = create a basic voice channel
+        $avatar opt**(user[notWorking])** = show's your avatar or any user of the guild*
+        """,inline=False)
         await msg.send(embed=embed)
 
 
